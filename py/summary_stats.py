2669765c49e0d43fb74a73b0e71f442238e716a0
mehtastable <jeremykquijano@gmail.com>
2026-05-03 01:10:51 -0500

    added requirements.txt

--- py/summary_stats.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("final_summary.csv")
-
-summary = df.groupby(["Generator", "Test"])["p-value"].agg(["mean", "std", "min", "max"])
-
-summary.to_csv("summary_stats.csv")
-
-print(summary)
